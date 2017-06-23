a155b26841541d292f10af5d38e8ad3456ba5b7c
Jakub Siwiński <kubsiw@gmail.com>
2017-06-23 22:48:01 +0200

    work started

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 jQuery(document).ready(function(){
     
+    var divNote = document.createElement('div');
+    
+    var button = document.querySelector('btn');
+    var section1 = document.querySelector('section1');
+    
+    
+    
 }); // end of DOM
